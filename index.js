d8ad42e1239e18250d06af386985ab7fd6479a38
Michael Schwartz <mykesx@gmail.com>
2019-03-15 08:53:02 -0700

    add support for nest protect

--- index.js
+++ index.js
@@ -1,35 +1,41 @@
-process.env.DEBUG = 'NestHost,HostBase';
+process.env.DEBUG = "ThermostatHost,HostBase";
 
-const
-      debug       = require('debug')('NestHost'),
-      request     = require('superagent'),
-      EventSource = require('eventsource'),
-      HostBase    = require('microservice-core/HostBase');
+const debug = require("debug")("ThermostatHost"),
+  request = require("superagent"),
+  EventSource = require("eventsource"),
+  HostBase = require("microservice-core/HostBase");
 
-const auth      = process.env.NEST_AUTH,
-      topicRoot = process.env.TOPIC_ROOT || 'nest',
-      mqttHost  = process.env.MQTT_HOST || 'mqtt://ha';
+const auth = process.env.NEST_AUTH,
+  topicRoot = process.env.TOPIC_ROOT || "nest",
+  mqttHost = process.env.MQTT_HOST || "mqtt://ha";
 
-class NestHost extends HostBase {
+/**
+ * Thermostat
+ */
+class ThermostatHost extends HostBase {
   constructor(structure, thermostat) {
-    super(mqttHost, topicRoot + '/' + structure.name + '/' + thermostat.name_long);
+    super(
+      mqttHost,
+      topicRoot + "/" + structure.name + "/" + thermostat.name_long
+    );
     try {
-      console.log('new NestHost', structure.name, thermostat.name);
+      console.log("new ThermostatHost", structure.name, thermostat.name);
       this.weather = structure.postal_code;
-      this.auth    = auth;
-    }
-    catch (e) {
+      this.auth = auth;
+    } catch (e) {
       console.log(e);
     }
   }
 
   async command(setting, value) {
-    const id  = this.state.device_id,
-          uri = `https://developer-api.nest.com/devices/thermostats/${id}?auth=${this.auth}`;
+    const id = this.state.device_id,
+      uri = `https://developer-api.nest.com/devices/thermostats/${id}?auth=${
+        this.auth
+      }`;
 
     debug(this.device, setting, value);
 
-    const o    = {};
+    const o = {};
     // Nest API is picky about numbers being JSON encoded as Numbers.
     o[setting] = isNaN(value) ? value : Number(value);
 
@@ -40,8 +46,7 @@ class NestHost extends HostBase {
         .end((error, result) => {
           if (error) {
             return reject(error.response.body);
-          }
-          else {
+          } else {
             return resolve(JSON.parse(result.text));
           }
         });
@@ -49,49 +54,71 @@ class NestHost extends HostBase {
   }
 }
 
+class ProtectHost extends HostBase {
+  constructor(structure, protect) {
+    super(mqttHost, topicRoot + "/" + structure.name + "/" + protect.name_long);
+  }
+  async command() {}
+}
+
 const hosts = {};
 
 async function connect() {
-  const eventSource = new EventSource('https://developer-api.nest.com?auth=' + auth);
+  const eventSource = new EventSource(
+    "https://developer-api.nest.com?auth=" + auth
+  );
 
-  eventSource.addEventListener('put', async (e) => {
-    const state = this.raw = JSON.parse(e.data).data,
-          devices = state.devices;
+  eventSource.addEventListener("put", async e => {
+    const state = (this.raw = JSON.parse(e.data).data),
+      devices = state.devices;
 
-    Object.keys(state.structures).forEach((id) => {
+    Object.keys(state.structures).forEach(id => {
       const s = state.structures[id];
 
-      s.thermostats.forEach(async (id) => {
-        const t   = devices.thermostats[id],
-              ndx = s.name + '/' + t.name_long;
+      s.thermostats.forEach(async id => {
+        const t = devices.thermostats[id],
+          ndx = s.name + "/" + t.name_long;
 
-        hosts[ndx]       = hosts[ndx] || new NestHost(s, t);
+        hosts[ndx] = hosts[ndx] || new ThermostatHost(s, t);
         hosts[ndx].state = Object.assign({}, t, {
           structure_name: s.name,
-          country_code:   s.country_code,
-          postal_code:    s.postal_code,
-          time_zone:      s.time_zone,
-          away:           s.away,
+          country_code: s.country_code,
+          postal_code: s.postal_code,
+          time_zone: s.time_zone,
+          away: s.away,
           rhr_enrollment: s.rhr_enrollment,
-          eta_begin:      s.eta_begin,
+          eta_begin: s.eta_begin
+        });
+      });
+      s.smoke_co_alarms.forEach(async id => {
+        const t = devices.smoke_co_alarms[id],
+          ndx = s.name + "/" + t.name_long;
 
+        hosts[ndx] = hosts[ndx] || new ProtectHost(s, t);
+        hosts[ndx].state = Object.assign({}, t, {
+          structure_name: s.name,
+          country_code: s.country_code,
+          postal_code: s.postal_code,
+          time_zone: s.time_zone,
+          away: s.away,
+          rhr_enrollment: s.rhr_enrollment,
+          eta_begin: s.eta_begin
         });
       });
     });
   });
 
-  eventSource.addEventListener('auth_revoked', (e) => {
-    debug('auth_revoked', e);
+  eventSource.addEventListener("auth_revoked", e => {
+    debug("auth_revoked", e);
   });
 
-  eventSource.addEventListener('open', (e) => {
-    debug('eventsource opened', e);
+  eventSource.addEventListener("open", e => {
+    debug("eventsource opened", e);
   });
 
-  eventSource.addEventListener('error', (e) => {
-    debug('eventsource error', e);
+  eventSource.addEventListener("error", e => {
+    debug("eventsource error", e);
   });
 }
 
 connect();
-
